e988fdcca4d2fab39ce97bcc689af72dc36fad5a
Mike Pottebaum <mpottebaum@gmail.com>
2020-01-27 03:39:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,8 @@ def long_planeteer_calls(calls)
   calls.any? {|n| n.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.
 end
