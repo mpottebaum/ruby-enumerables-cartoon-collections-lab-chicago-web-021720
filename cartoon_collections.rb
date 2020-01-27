55ac35a66c838fb621c63a08f998a40506a68f26
Mike Pottebaum <mpottebaum@gmail.com>
2020-01-27 03:36:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ def summon_captain_planet(calls)
   calls.map {|n| "#{n.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? {|n| n.length > 4}
 end
 
 def find_the_cheese# code an argument here
