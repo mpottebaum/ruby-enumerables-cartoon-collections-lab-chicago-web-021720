abb2aab0a49423313745b3bb1c45292f78796421
Mike Pottebaum <mpottebaum@gmail.com>
2020-01-27 03:34:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,10 @@ def roll_call_dwarves(names_array)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  capbang = calls.map do |n|
+    "#{n.capitalize}"
+  end
 end
 
 def long_planeteer_calls# code an argument here
